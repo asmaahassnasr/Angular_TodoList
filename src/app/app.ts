9c6f702d48cb769ca83412b2800db9a55e049c9e
ASM@GitHub2024 <asmaa14has@gmail.com>
2026-07-19 10:20:37 +0300

    creating todo component and use it in app ts and html files

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import {MatButtonModule} from '@angular/material/button'
+import { ToDo } from './components/to-do/to-do';
 
 @Component({
   selector: 'app-root',
-  imports: [MatButtonModule,],
+  imports: [MatButtonModule, ToDo],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
---
+++ src/app/components/to-do/to-do.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-to-do',
+  imports: [],
+  templateUrl: './to-do.html',
+  styleUrl: './to-do.scss',
+})
+export class ToDo {}
