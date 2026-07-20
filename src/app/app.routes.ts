ff3786f28f3cf6a46f057775dd0d0b090a5fbebf
ASM@GitHub2024 <asmaa14has@gmail.com>
2026-07-21 02:04:45 +0300

    make routes for all pages needed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { Home } from './components/home/home';
+import { AboutUs } from './components/about-us/about-us';
+import { Contact } from './components/contact/contact';
+import { TodosWithJsonServer } from './components/todos-with-json-server/todos-with-json-server';
+import { TodosDetails } from './components/todos-details/todos-details';
+import { NotFound } from './components/not-found/not-found';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:'' , redirectTo:"/home" , pathMatch:'full'},
+  {path:'home' , component:Home , title:'Home PAge '},
+  {path:'about' , component:AboutUs , title:'About Us PAge '},
+  {path:'contact' , component:Contact , title:'Contact Us PAge '},
+  {path:'todos' , component:TodosWithJsonServer , title:'Todos PAge '},
+  {path:'todos/:id' , component:TodosDetails , title:'Todo Details PAge '},
+  {path:'**' , component:NotFound , title:'Not Found PAge '},
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,15 @@
 import { Component, signal } from '@angular/core';
-import { ToDo } from './components/to-do/to-do';
+// import { ToDo } from './components/to-do/to-do';
+import { RouterOutlet } from '@angular/router';
+import { Header } from './components/header/header';
+import { Footer } from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [ ToDo],
+  imports: [
+  RouterOutlet , Header, Footer,
+    //  ToDo
+    ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
