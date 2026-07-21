036e662d4bf242b619599dfca646b8e06900a0fc
ASM@GitHub2024 <asmaa14has@gmail.com>
2026-07-21 03:31:02 +0300

    Show all Todos from server

--- src/app/components/todos-with-json-server/todos-with-json-server.ts
+++ src/app/components/todos-with-json-server/todos-with-json-server.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ITodo } from '../../Models/itodo';
+import { TodoService } from '../../Services/todo-service';
 
 @Component({
   selector: 'app-todos-with-json-server',
@@ -6,4 +8,20 @@ import { Component } from '@angular/core';
   templateUrl: './todos-with-json-server.html',
   styleUrl: './todos-with-json-server.scss',
 })
-export class TodosWithJsonServer {}
+export class TodosWithJsonServer implements OnInit {
+
+  todos : ITodo[] = [];
+  newTodo : ITodo = {} as ITodo ;
+
+  constructor(private _todosService:TodoService){}
+
+  ngOnInit(): void {
+    this.getAllTodos();
+  }
+
+  getAllTodos(){
+    this._todosService.getTodos().subscribe(todos => {
+      this.todos = todos;
+    })
+  }
+}
