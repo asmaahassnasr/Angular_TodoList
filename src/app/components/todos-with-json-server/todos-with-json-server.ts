24c20a2ef72adc4d0496e4127a4faf4e87db2341
ASM@GitHub2024 <asmaa14has@gmail.com>
2026-07-21 04:26:55 +0300

    CRUD operations with APIS

--- src/app/components/todos-with-json-server/todos-with-json-server.ts
+++ src/app/components/todos-with-json-server/todos-with-json-server.ts
@@ -1,27 +1,52 @@
 import { Component, OnInit } from '@angular/core';
 import { ITodo } from '../../Models/itodo';
 import { TodoService } from '../../Services/todo-service';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-todos-with-json-server',
-  imports: [],
+  imports: [FormsModule, CommonModule, RouterModule],
   templateUrl: './todos-with-json-server.html',
   styleUrl: './todos-with-json-server.scss',
 })
 export class TodosWithJsonServer implements OnInit {
+  todos: ITodo[] = [];
+  newTodo: ITodo = {} as ITodo;
 
-  todos : ITodo[] = [];
-  newTodo : ITodo = {} as ITodo ;
-
-  constructor(private _todosService:TodoService){}
+  constructor(private _todosService: TodoService) {}
 
   ngOnInit(): void {
     this.getAllTodos();
   }
 
-  getAllTodos(){
-    this._todosService.getTodos().subscribe(todos => {
+  getAllTodos() {
+    this._todosService.getTodos().subscribe((todos) => {
       this.todos = todos;
-    })
+    });
   }
+
+  createTodo(): void {
+    const createdTodo: ITodo = {
+      id: this.newTodo.id,
+      title: this.newTodo.title,
+      isCompleted: false,
+    };
+    this.newTodo = createdTodo;
+    this._todosService.createTodo(createdTodo).subscribe((todo) => {
+      this.todos.push(todo);
+    });
+  }
+
+  deleteTodo(todoId: string): ITodo[] {
+    this._todosService.deleteTodo(todoId).subscribe(() => {
+      this.todos =  this.todos.filter((todo) => todo.id !== todoId);
+    });
+
+    return this.todos;
+  }
+
+
+
 }
